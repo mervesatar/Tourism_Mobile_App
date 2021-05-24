156f5077b270ca21602066ffb77041f482a2fb2a
akadirbozoglu <56932627+akadirbozoglu@users.noreply.github.com>
2021-05-24 20:32:37 +0300

    Added Unity Project
    Unity 2020.3.8f1
    -- Changes in Project Settings--
    + Minimum API Level 21
    + Target API Level 30
    - Vulcan Graphics API
    + OpenGLES' Graphics API
    + Scripting Backend: Mono -> IL2CPP
    +Editor File (Exporting Flutter configuration)
    + Rotate.cs

---
+++ unity/ARtest/Assets/Scripts/Rotate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Rotate : MonoBehaviour, IEventSystemHandler
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Vector3 RotateAmount;
+
+    void Start()
+    {
+        RotateAmount = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(RotateAmount * Time.deltaTime * 10);
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (Input.GetTouch(i).phase.Equals(TouchPhase.Began))
+            {
+                var hit = new RaycastHit();
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    //message
+                }
+            }
+        }
+    }
+
+    public void SetRotationSpeed(String message)
+    {
+        float value = float.Parse(message);
+        RotateAmount = new Vector3(value, value, value);
+    }
+}
+
