0746a91a91b6b0da18e4559f416909c237c9456b
mustafademiroz <mustafa.demiroz@agu.edu.tr>
2021-06-17 15:32:46 +0300

    Fix in Sign up

--- lib/signup.dart
+++ lib/signup.dart
@@ -17,7 +17,9 @@ class _signup extends State<signup> {
   TextEditingController ageControl = new TextEditingController();
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  String interest = 'history';
 
+  var items = ['history', 'food', 'fun', 'art'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -125,15 +127,40 @@ class _signup extends State<signup> {
                 height: (MediaQuery.of(context).size.height) / 30,
               ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
-                child: TextField(
-                  controller: interestControl,
-                  decoration: InputDecoration(
-                      fillColor: Colors.white.withOpacity(0.7),
-                      filled: true,
-                      border: OutlineInputBorder(),
-                      labelText: 'Interest',
-                      hintText: 'fun,history,art,food'),
+                padding: const EdgeInsets.fromLTRB(20, 8, 20, 0),
+                child: new Container(
+
+                  child: new Column(
+                    children: [
+                      new Row(
+                        children: <Widget>[
+                          new Expanded(
+                              child:
+                              new TextField(controller: interestControl,
+                                decoration: InputDecoration(
+                                  fillColor: Colors.white.withOpacity(0.7),
+                                  filled: true,
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Interest',
+                                ),
+                              )),
+                          new PopupMenuButton<String>(
+                            icon: const Icon(Icons.arrow_drop_down),
+                            onSelected: (String value) {
+                              interestControl.text = value;
+                            },
+                            itemBuilder: (BuildContext context) {
+                              return items
+                                  .map<PopupMenuItem<String>>((String value) {
+                                return new PopupMenuItem(
+                                    child: new Text(value), value: value);
+                              }).toList();
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
               SizedBox(
